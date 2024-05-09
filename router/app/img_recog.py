ead00cebb5d96a7ba29b85ece5382742bff57d88
IsaiasCBLuz <132511426+IsaiasCBLuz@users.noreply.github.com>
2024-05-09 09:41:05 -0300

    Implementação do ChatGPT para recog de imgs

--- router/app/img_recog.py
+++ router/app/img_recog.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import JSONResponse
 import json
 import os
+from openai import OpenAI
 # Import db connection
 from database.conexao import Conexao
 from database.sqlalchemy import Personalizacao
@@ -24,8 +25,37 @@ router = APIRouter(
 async def verificar_imagem(request: Request):
     session = Conexao().session
     try:
-        # Aqui vem o seu código
-        return JSONResponse(content={"message": "Imagem verificada com sucesso!"}) # Talvez seja necessário alterar o return
+        data = await request.json()
+
+        client = OpenAI()
+
+        response = client.chat.completions.create(
+          model="gpt-4-turbo",
+          messages=[
+            {
+              "role": "user",
+              "content": [
+                {"type": "text", 
+                 "text": 
+                 "Please provide an image of a food item. Upon analysis, the system will return a dictionary with the following structure: dict = {'food': '', 'group': ''} Where: 'food' receives the name of the food contained in the image. And 'group' receives the food group to which this item belongs, and can take one of the following values: Fruits, Vegetables and greens, Meat and eggs, Cereals, tubers, bread and roots, Legumes, Milk and dairy products, Sweets, Snacks, Fungus. If the image is not a food, 'Not a food' will be returned for both key values. I just want you to return me a dictionary already filled"},
+                {
+                  "type": "image_url",
+                  "image_url": {
+                    "url": data["imagem"],
+                  },
+                },
+              ],
+            }
+          ],
+          max_tokens=100,
+        )
+
+        # "```json\n{\n  \"food\": \"Grilled salmon\",\n  \"group\": \"Meat and eggs\"\n}\n```"
+        # "```json\n{\n  \"food\": \"Feijoada\",\n  \"group\": \"Legumes\"\n}\n```"
+        
+        teste = response.choices[0]
+        return teste
+        return JSONResponse(content={"message": "Imagem verificada com sucesso!"})
     except Exception as e:
         return JSONResponse(content={"message": "Erro ao verificar a imagem!", "error": str(e)})
     finally:
