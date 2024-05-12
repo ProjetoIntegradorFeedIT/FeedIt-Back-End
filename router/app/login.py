1043da97a81950a0446f01af092fd576663c2786
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-05-12 00:50:45 -0300

    Ajuste no sistema de criptografia e verificacao

--- router/app/login.py
+++ router/app/login.py
@@ -15,11 +15,32 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-# Função para descriptografar a senha que vem do banco de dados
-def descriptografa_senha(stored_password, stored_salt, password_to_check):
-    hash_to_check = hashlib.pbkdf2_hmac('sha256', password_to_check.encode('utf-8'), stored_salt, 100000)
-    return hash_to_check == stored_password
+# Função para criptografar a senha
+def criptografar_senha(senha):
+    # Codificando a senha em bytes
+    senha_bytes = senha.encode('utf-8')
 
+    # Criando um objeto de hash usando o algoritmo SHA-256
+    sha256 = hashlib.sha256()
+
+    # Atualizando o objeto de hash com a senha codificada
+    sha256.update(senha_bytes)
+
+    # Gerando o hash da senha e retornando como hexadecimal
+    senha_hash = sha256.hexdigest()
+    
+    return senha_hash
+
+# Função para descriptografar a senha que vem do banco de dados
+def verificar_senha(senha, senha_hash):
+    # Criptografando a senha fornecida
+    senha_fornecida_hash = criptografar_senha(senha)
+
+    # Verificando se a senha fornecida e a senha armazenada são iguais
+    if senha_fornecida_hash == senha_hash:
+        return True
+    else:
+        return False
 # Depois remove o comentário
 # O que fazer: Criar uma rota para autenticar um usuário
 # Como? - Criar um endpoint POST /login que recebe um JSON com o email e a senha do usuário
