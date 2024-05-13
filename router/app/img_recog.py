bc90ba3310c456ccf2dda036f6579fd8a426cc38
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-05-13 17:01:46 -0300

    base64

--- router/app/img_recog.py
+++ router/app/img_recog.py
@@ -1,17 +1,19 @@
 # Imports
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 import json
 import os
 from openai import OpenAI
+import base64
+
 # Import db connection
 from database.conexao import Conexao
 from database.sqlalchemy import Personalizacao
 
 # Router
 router = APIRouter(
-    prefix = "/img_recog",
-    tags = ["Imagem_Reconhecimento"],
+    prefix="/img_recog",
+    tags=["Imagem_Reconhecimento"],
     responses={404: {"description": "Not found"}},
 )
 
@@ -23,29 +25,30 @@ async def verificar_imagem(request: Request):
 
         client = OpenAI()
 
-        response = client.chat.completions.create(
-          model="gpt-4-turbo",
-          messages=[
-            {
-              "role": "user",
-              "content": [
-                {"type": "text", 
-                 "text": 
-                 "Please provide an image of a food item. Upon analysis, the system will return a dictionary with the following structure: dict = {'food': '', 'group': ''} Where: 'food' receives the name of the food contained in the image. And 'group' receives the food group to which this item belongs, and can take one of the following values: Fruits, Vegetables and greens, Meat and eggs, Cereals, tubers, bread and roots, Legumes, Milk and dairy products, Sweets, Snacks, Fungus. If the image is not a food, 'Not a food' will be returned for both key values. I just want you to return me a dictionary already filled"},
-                {
-                  "type": "image_url",
-                  "image_url": {
-                    "url": data["imagem"],
-                  },
-                },
-              ],
-            }
-          ],
-          max_tokens=300,
-        )
+        # Decode the base64 image data
+        image_data = base64.b64decode(data["imagem"])
 
-        # string = "```json\n{\n  \"food\": \"Grilled salmon\",\n  \"group\": \"Meat and eggs\"\n}\n```"
-        # string = "```json\n{\n  \"food\": \"Feijoada\",\n  \"group\": \"Legumes\"\n}\n```"
+        response = client.chat.completions.create(
+            model="gpt-4-turbo",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Please provide an image of a food item. Upon analysis, the system will return a dictionary with the following structure: dict = {'food': '', 'group': ''} Where: 'food' receives the name of the food contained in the image. And 'group' receives the food group to which this item belongs, and can take one of the following values: Fruits, Vegetables and greens, Meat and eggs, Cereals, tubers, bread and roots, Legumes, Milk and dairy products, Sweets, Snacks, Fungus. If the image is not a food, 'Not a food' will be returned for both key values. I just want you to return me a dictionary already filled",
+                        },
+                        {
+                            "type": "image",
+                            "image": {
+                                "data": image_data,
+                            },
+                        },
+                    ],
+                }
+            ],
+            max_tokens=300,
+        )
 
         string = response.choices[0].message.content
         formata = eval(string.replace('```json\n', '').replace('\n```', ''))
@@ -53,10 +56,7 @@ async def verificar_imagem(request: Request):
         if formata["food"] == "Not a food":
             return JSONResponse(content={"message": "Imagem não é um alimento!"})
 
-        
-        teste = response.choices[0]
         return formata
-        return JSONResponse(content={"message": "Imagem verificada com sucesso!"})
     except Exception as e:
         return JSONResponse(content={"message": "Erro ao verificar a imagem!", "error": str(e)})
     finally:
