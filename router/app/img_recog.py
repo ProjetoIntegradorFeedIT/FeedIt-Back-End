7f526c28f25b77b99d492af9fb5f3f0074592aed
IsaiasCBLuz <isaiascbluz@gmail.com>
2024-05-31 01:35:46 -0300

    input atravez de seleção, concerto de retornos

--- router/app/img_recog.py
+++ router/app/img_recog.py
@@ -105,11 +105,64 @@ async def verificar_imagem(request: Request):
 
             valor_alimento = re.search(r"'comida': '([^']*)'", resposta).group(1)
 
-            return JSONResponse(content={"status_code": 200, "message": "Imagem verificada com sucesso!", "grupo": valor_grupo, "alimento": valor_alimento})
+            return JSONResponse(content={"status_code": 200, "message": "Imagem verificada com sucesso!", "grupo": valor_grupo, "alimento": valor_alimento, "alimentacaoSaudavel": select_grupo.alimentacao, "forca": select_grupo.forca, "felicidade": select_grupo.felicidade, "energia": select_grupo.energia})
         else:
             return JSONResponse(content={"status_code": 304, "message": "Opa, parece que isso não é um alimento, se a gente se enganou conta ai pra gente o que que é isso!"})
         
     except Exception as e:
         return JSONResponse(content={"message": "Erro ao verificar a imagem!", "error": str(e), "status_code": 500})
+    finally:
+        session.close()
+
+@router.post("/forceInput")
+async def force_input(request: Request):
+    session = Conexao().session
+    try:
+        data = await request.json()
+
+        select_grupo = session.query(GrupoAlimentos).filter(GrupoAlimentos.grupo == data["grupo"]).first()
+        select_pet = session.query(Pet).filter(Pet.id_crianca == data["id_crianca"]).first()
+        if (select_pet.alimentacao + select_grupo.alimentacao) > 100:
+            select_pet.alimentacao = 100
+            session.commit()
+        elif (select_pet.alimentacao + select_grupo.alimentacao) < -100:
+            select_pet.alimentacao = -100
+            session.commit()
+        else:
+            select_pet.alimentacao += select_grupo.alimentacao
+            session.commit()
+        if (select_pet.forca + select_grupo.forca) > 100:
+            select_pet.forca = 100
+            session.commit()
+        elif (select_pet.forca + select_grupo.forca) < -100:
+            select_pet.forca = -100
+            session.commit()
+        else:
+            select_pet.forca += select_grupo.forca
+            session.commit()
+        if (select_pet.felicidade + select_grupo.felicidade) > 100:
+            select_pet.felicidade = 100
+            session.commit()
+        elif (select_pet.felicidade + select_grupo.felicidade) < -100:
+            select_pet.felicidade = -100
+            session.commit()
+        else:
+            select_pet.felicidade += select_grupo.felicidade
+            session.commit()
+        if (select_pet.energia + select_grupo.energia) > 100:
+            select_pet.energia = 100
+            session.commit()
+        elif (select_pet.energia + select_grupo.energia) < -100:
+            select_pet.energia = -100
+            session.commit()
+        else:
+            select_pet.energia += select_grupo.energia
+            session.commit()
+        insert = PetGrupoAlimento(id_pet=select_pet.id_pet, id_grupo=select_grupo.id)
+        session.add(insert)
+
+        return JSONResponse(content={"status_code": 200, "message": "Forçando input de dados!", "alimentacaoSaudavel": select_grupo.alimentacao, "forca": select_grupo.forca, "felicidade": select_grupo.felicidade, "energia": select_grupo.energia})
+    except Exception as e:
+        return JSONResponse(content={"message": "Erro ao forçar input de dados!", "error": str(e), "status_code": 500})
     finally:
         session.close()
